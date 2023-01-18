ea6e94aac586d9620c6cb9e70cb4b0f31420cdfb
munirahassan789 <121326918+munirahassan789@users.noreply.github.com>
2023-01-18 14:05:35 -0500

    added new

--- Portfolio(Munira_Hassan) updated 2/js/scripts.js
+++ Portfolio(Munira_Hassan) updated 2/js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Thanks for checking out my code! - Munira Hassan");
+
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
